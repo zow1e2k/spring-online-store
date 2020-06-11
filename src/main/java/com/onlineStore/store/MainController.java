b8eaf770af24d5768eb0651ff9a2dcce4419b0ff
Artyom <kadomodify@gmail.com>
2020-06-11 03:11:19 +0300

    +maria db 10.3.13, create & cfg jdbc

--- src/main/java/com/onlineStore/store/MainController.java
+++ src/main/java/com/onlineStore/store/MainController.java
@@ -1,14 +1,61 @@
 package com.onlineStore.store;
 
+import com.onlineStore.store.domain.Message;
+import com.onlineStore.store.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MainController {
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping
-    public String main(Model model) {
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(
+            @RequestParam(name="text", required=true, defaultValue="")
+                    String text,
+            @RequestParam(name="tag", required=true, defaultValue="")
+                    String tag,
+            Map<String, Object> model)
+    {
+        Message msg = new Message(text, tag);
+        messageRepo.save(msg);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(
+            @RequestParam(name="filter", required=true, defaultValue="")
+                    String filter,
+            Map<String, Object> model)
+    {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
         return "main";
     }
 
---
+++ src/main/java/com/onlineStore/store/domain/Message.java
@@ -0,0 +1,45 @@
+package com.onlineStore.store.domain;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String text;
+    private String tag;
+
+    public Message() { }
+
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+}
